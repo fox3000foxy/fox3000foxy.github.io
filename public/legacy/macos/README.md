53c124f727042deb756e637edf9e9d301ce7807f
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-03-20 09:20:11 +0100

    deported legacy

--- public/legacy/macos/README.md
+++
@@ -1,2 +0,0 @@
-# macOS-HTML
-A recreation of macOS in HTML, based off of my Win11-HTML repository.
--- public/legacy/macos/scripts/applist.js
+++
@@ -1,2 +0,0 @@
-createappwithdockicon("messages", "https://bluebubbles.app/web/", "img/messages.png", "Messages")
-createapp("vscode", "https://vscode.dev", "img/vscode.png", "VS Code")
