b4562030891adb6612516f9005623d67607f176b
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-05-29 14:41:21 +0200

    fix formatting for AuthorIndex and AuthorBio

--- src/components/AuthorBio.tsx
+++ src/components/AuthorBio.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { getAuthors, type Author } from "../utils/authors";
 
 export default function AuthorBio({ authors }: { authors?: string[] }) {
@@ -6,7 +7,11 @@ export default function AuthorBio({ authors }: { authors?: string[] }) {
 	return (
 		<div className="author-bio-top">
 			{list.map((author: Author) => (
-				<div key={author.id} className="author-bio-item">
+				<Link
+					key={author.id}
+					to={`/authors/${author.id}`}
+					className="author-bio-item"
+				>
 					<img
 						className="author-avatar-small"
 						src={author.avatar ?? `https://github.com/${author.id}.png`}
@@ -14,16 +19,9 @@ export default function AuthorBio({ authors }: { authors?: string[] }) {
 					/>
 					<div className="author-info-top">
 						<span className="author-name-top">{author.name}</span>
-						<a
-							href={author.github}
-							target="_blank"
-							rel="noopener noreferrer"
-							className="author-link-top"
-						>
-							@{author.id}
-						</a>
+						<span className="author-link-top">@{author.id}</span>
 					</div>
-				</div>
+				</Link>
 			))}
 		</div>
 	);
