1d7b190a9cadb560dcf9fdafd0bad5d456341201
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-05-29 14:03:26 +0200

    add per-article OG images (SVG), mobile responsive header & article layout

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'
+            return 'vendor';
           }
         },
       },
