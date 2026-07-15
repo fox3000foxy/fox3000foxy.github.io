07e36c94362e9f846518f80db282267391f2db62
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-07-15 11:42:14 +0200

    formatted file

--- src/pages/news-sitemap.xml.ts
+++ src/pages/news-sitemap.xml.ts
@@ -3,19 +3,27 @@ import path from "node:path";
 import { SITE_URL } from "../lib/i18n";
 
 function escapeXml(s: string): string {
-  return s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+	return s
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&apos;");
 }
 
 export function GET() {
-  const site = SITE_URL;
-  let articles: { slug?: string; title?: string; date?: string }[] = [];
-  try {
-    articles = JSON.parse(fs.readFileSync(path.resolve("public/articles/en/index.json"), "utf-8"));
-  } catch {}
+	const site = SITE_URL;
+	let articles: { slug?: string; title?: string; date?: string }[] = [];
+	try {
+		articles = JSON.parse(
+			fs.readFileSync(path.resolve("public/articles/en/index.json"), "utf-8")
+		);
+	} catch {}
 
-  const urlEntries = articles
-    .filter((a) => a.date)
-    .map((a) => `  <url>
+	const urlEntries = articles
+		.filter((a) => a.date)
+		.map(
+			(a) => `  <url>
     <loc>${escapeXml(`${site}/blog/${a.slug}`)}</loc>
     <news:news>
       <news:publication>
@@ -25,15 +33,16 @@ export function GET() {
       <news:publication_date>${a.date}</news:publication_date>
       <news:title>${escapeXml(a.title || a.slug || "")}</news:title>
     </news:news>
-  </url>`)
-    .join("\n");
+  </url>`
+		)
+		.join("\n");
 
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>
+	return new Response(
+		`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
 ${urlEntries}
 </urlset>`,
-    { headers: { "Content-Type": "application/xml; charset=utf-8" } }
-  );
+		{ headers: { "Content-Type": "application/xml; charset=utf-8" } }
+	);
 }
