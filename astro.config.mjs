e63b0c195e22275833a46c437c2bdb87a615a060
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-08-03 12:24:53 +0200

    feat: exclude express-honeypot pages from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   site: SITE_URL,
   output: "static",
   integrations: [
-    sitemap({ filter: (page) => !page.includes("/write") && !page.includes("/api/"), changefreq: "weekly", priority: 0.7, lastmod: new Date() }),
+    sitemap({ filter: (page) => !page.includes("/write") && !page.includes("/api/") && !page.includes("express-honeypot"), changefreq: "weekly", priority: 0.7, lastmod: new Date() }),
     preact({ compat: true }),
   ],
   image: {
