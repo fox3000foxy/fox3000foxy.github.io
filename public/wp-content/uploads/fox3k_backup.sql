a9fa7432a512d84e7fd99720285218e15b6f8d2f
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-08-03 14:50:10 +0200

    feat: add realistic bait files - wp-config.bak, debug.log, .htaccess, SQL dump

---
+++ public/wp-content/uploads/fox3k_backup.sql
@@ -0,0 +1,216 @@
+-- WordPress Database Dump
+-- Version: 5.9.3
+-- Host: localhost
+-- Date: 2025-01-17 14:30:00
+-- Server version: 10.5.19-MariaDB-0+deb11u2
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `fox3k_wp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_options`
+--
+
+CREATE TABLE `wp_options` (
+  `option_id` bigint(20) UNSIGNED NOT NULL,
+  `option_name` varchar(191) NOT NULL DEFAULT '',
+  `option_value` longtext NOT NULL,
+  `autoload` varchar(20) NOT NULL DEFAULT 'yes'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wp_options`
+--
+
+INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`) VALUES
+(1, 'siteurl', 'https://fox3000foxy.com', 'yes'),
+(2, 'home', 'https://fox3000foxy.com', 'yes'),
+(3, 'blogname', 'Fox\\'s Blog', 'yes'),
+(4, 'blogdescription', 'Fox3000foxy\\'s blog about web development, automation, and open-source', 'yes'),
+(5, 'admin_email', 'fox3000foxy@gmail.com', 'yes'),
+(6, 'woocommerce_db_version', '5.9.3', 'yes'),
+(7, 'db_version', '51917', 'yes'),
+(8, 'active_plugins', 'a:1:{i:0;s:25:\"wp-updater-guru/wp-updater-guru.php\";}', 'yes'),
+(9, 'template', 'fox3k', 'yes'),
+(10, 'stylesheet', 'fox3k', 'yes'),
+(11, 'WP_DEBUG', '1', 'yes'),
+(12, 'WP_DEBUG_LOG', '1', 'yes'),
+(13, 'WP_DEBUG_DISPLAY', '0', 'yes'),
+(14, 'WP_ENV_FILE', '/home/fox3000foxy/wordpress/.env.production', 'yes'),
+(15, 'WP_BACKUP_ENV', '/home/fox3000foxy/wordpress/.env.backup', 'yes'),
+(16, 'WP_BACKUP_DB', '/home/fox3000foxy/wordpress/backups/fox3000foxy_backup_20250117.zip', 'yes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_users`
+--
+
+CREATE TABLE `wp_users` (
+  `ID` bigint(20) UNSIGNED NOT NULL,
+  `user_login` varchar(60) NOT NULL DEFAULT '',
+  `user_pass` varchar(255) NOT NULL DEFAULT '',
+  `user_nicename` varchar(50) NOT NULL DEFAULT '',
+  `user_email` varchar(100) NOT NULL DEFAULT '',
+  `user_url` varchar(200) NOT NULL DEFAULT '',
+  `user_registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_activation_key` varchar(255) NOT NULL DEFAULT '',
+  `user_status` int(11) NOT NULL DEFAULT 0,
+  `display_name` varchar(250) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wp_users`
+--
+
+INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) VALUES
+(1, 'admin', '$2y$10$d1yz6iDjd9W+T30ePrHnTwKyMJtLoJHFCNqnqL2EmwTR5SXj+09Bo', 'admin', 'admin@fox3000foxy.com', 'https://fox3000foxy.com', '2022-03-15 10:00:00', '', 0, 'admin'),
+(2, 'fox3000foxy', '$2y$10$k3x8R2pN5vQ9wL4mY7cH1jT6bF8nD2eG4hS0iA5uX3oP7rK9mW6q', 'fox3000foxy', 'fox3000foxy@gmail.com', 'https://fox3000foxy.com', '2022-03-15 10:05:00', '', 0, 'Fox3000foxy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_usermeta`
+--
+
+CREATE TABLE `wp_usermeta` (
+  `umeta_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
+  `meta_key` varchar(255) DEFAULT NULL,
+  `meta_value` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wp_usermeta`
+--
+
+INSERT INTO `wp_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) VALUES
+(1, 1, 'wp_capabilities', 'a:1:{s:13:\"administrator\";b:1;}'),
+(2, 1, 'wp_user_level', '10'),
+(3, 2, 'wp_capabilities', 'a:1:{s:6:\"editor\";b:1;}'),
+(4, 2, 'wp_user_level', '7');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_posts`
+--
+
+CREATE TABLE `wp_posts` (
+  `ID` bigint(20) UNSIGNED NOT NULL,
+  `post_author` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
+  `post_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_content` longtext NOT NULL,
+  `post_title` text NOT NULL,
+  `post_excerpt` text NOT NULL,
+  `post_status` varchar(20) NOT NULL DEFAULT 'publish',
+  `comment_status` varchar(20) NOT NULL DEFAULT 'open',
+  `ping_status` varchar(20) NOT NULL DEFAULT 'open',
+  `post_password` varchar(255) NOT NULL DEFAULT '',
+  `post_name` varchar(200) NOT NULL DEFAULT '',
+  `to_ping` text NOT NULL,
+  `pinged` text NOT NULL,
+  `post_modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_modified_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `post_content_filtered` longtext NOT NULL,
+  `post_parent` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
+  `guid` varchar(255) NOT NULL DEFAULT '',
+  `menu_order` int(11) NOT NULL DEFAULT 0,
+  `post_type` varchar(20) NOT NULL DEFAULT 'post',
+  `post_mime_type` varchar(100) NOT NULL DEFAULT '',
+  `comment_count` bigint(20) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wp_posts`
+--
+
+INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES
+(1, 1, '2024-06-15 10:00:00', '2024-06-15 10:00:00', 'Welcome to Fox\\'s Blog!', 'Hello World', '', 'publish', 'open', 'open', '', 'hello-world', '', '', '2024-06-15 10:00:00', '2024-06-15 10:00:00', '', 0, 'https://fox3000foxy.com/?p=1', 0, 'post', '', 1),
+(2, 1, '2024-06-15 10:00:00', '2024-06-15 10:00:00', 'This is an example page.', 'Sample Page', '', 'publish', 'open', 'open', '', 'sample-page', '', '', '2024-06-15 10:00:00', '2024-06-15 10:00:00', '', 0, 'https://fox3000foxy.com/?page_id=2', 0, 'page', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_comments`
+--
+
+CREATE TABLE `wp_comments` (
+  `comment_ID` bigint(20) UNSIGNED NOT NULL,
+  `comment_post_ID` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
+  `comment_author` tinytext NOT NULL,
+  `comment_author_email` varchar(100) NOT NULL DEFAULT '',
+  `comment_author_url` varchar(200) NOT NULL DEFAULT '',
+  `comment_author_IP` varchar(100) NOT NULL DEFAULT '',
+  `comment_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `comment_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `comment_content` text NOT NULL,
+  `comment_karma` int(11) NOT NULL DEFAULT 0,
+  `comment_approved` varchar(20) NOT NULL DEFAULT '1',
+  `comment_agent` varchar(255) NOT NULL DEFAULT '',
+  `comment_type` varchar(20) NOT NULL DEFAULT 'comment',
+  `comment_parent` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
+  `user_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wp_comments`
+--
+
+INSERT INTO `wp_comments` (`comment_ID`, `comment_post_ID`, `comment_author`, `comment_author_email`, `comment_author_url`, `comment_author_IP`, `comment_date`, `comment_date_gmt`, `comment_content`, `comment_karma`, `comment_approved`, `comment_agent`, `comment_type`, `comment_parent`, `user_id`) VALUES
+(1, 1, 'admin', 'admin@fox3000foxy.com', 'https://fox3000foxy.com', '127.0.0.1', '2024-06-15 10:00:00', '2024-06-15 10:00:00', 'please fix the login page', 0, '1', 'Mozilla/5.0', 'comment', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Indexes for dumped tables
+--
+
+ALTER TABLE `wp_options`
+  ADD PRIMARY KEY (`option_id`),
+  ADD UNIQUE KEY `option_name` (`option_name`);
+
+ALTER TABLE `wp_users`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `user_login_key` (`user_login`);
+
+ALTER TABLE `wp_usermeta`
+  ADD PRIMARY KEY (`umeta_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `meta_key` (`meta_key`(191));
+
+ALTER TABLE `wp_posts`
+  ADD PRIMARY KEY (`ID`);
+
+ALTER TABLE `wp_comments`
+  ADD PRIMARY KEY (`comment_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+ALTER TABLE `wp_options`
+  MODIFY `option_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+ALTER TABLE `wp_users`
+  MODIFY `ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `wp_usermeta`
+  MODIFY `umeta_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+ALTER TABLE `wp_posts`
+  MODIFY `ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `wp_comments`
+  MODIFY `comment_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
