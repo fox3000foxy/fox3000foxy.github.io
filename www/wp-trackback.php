9b76923c85c34cfd2271de8393c701b790eb31f4
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-08-14 11:21:36 +0200

    chore: zip honeypot www to reduce repo size (114MB → 34MB)

--- www/wp-trackback.php
+++
@@ -1,5 +0,0 @@
-<?php
-// WordPress Pingback / Trackback endpoint. Deprecated but kept enabled.
-header('Content-Type: text/xml; charset=UTF-8');
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<methodResponse><params><param><value><int>0</int></value></param></params></methodResponse>';
